692c0c04529207d4dfa9cfe188ec7a8392ae62f0
Vielman Paredes <vparedes@intelix.biz>
2022-03-28 19:01:48 -0400

    Fundamentos de Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
+import { PersonasComponrnt } from './personas/personas.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, PersonasComponrnt, PersonaComponent
   ],
   imports: [
     BrowserModule
